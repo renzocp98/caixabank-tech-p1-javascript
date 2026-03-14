fc0946a6959ca66c906cc5fe6c4ec6e4df4e48bd
renzocp98 <renzo0ncp@gmail.com>
2026-03-15 00:33:56 +0100

    Crear y definir Clase Events

---
+++ events.js
@@ -0,0 +1,10 @@
+export class Events{
+    constructor(name, url, location, sale, date, classification){
+        this.name = name;
+        this.url = url;
+        this.location;
+        this.sale;
+        this.date;
+        this.classification
+    }
+}
