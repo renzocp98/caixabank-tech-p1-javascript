c7ed8c8b3cade3bdc301a63b3f8ae2ffb8a1f172
renzocp98 <renzo0ncp@gmail.com>
2026-03-14 23:24:09 +0100

    prueba de llamada a API y mostrado por consola

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-import { writeFile } from "fs/promises";
-import { render } from "./render.js";
-import { loadUsers } from "./users.js";
+//import { writeFile } from "fs/promises";
+//import { render } from "./render.js";
+//import { loadUsers } from "./users.js";
+//
+//const users = await loadUsers(100);
+//const html = render(users);
+//await writeFile('users.html', html);
 
-const users = await loadUsers(100);
-const html = render(users);
-await writeFile('users.html', html);
+const response = await fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=IY15m5Po8rivnx1OIfoiX8lYZcYyTgVY&size=2');
+const data = await response.json();
 
+console.log(data._embedded.events);
+    
