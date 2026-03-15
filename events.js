5276cf0caa4e35fd61362cb05f437b65a635f77c
renzocp98 <renzo0ncp@gmail.com>
2026-03-15 13:46:15 +0100

    Modificación Clase Events y uso de fetch, prueba de obtención de datos correctos

--- events.js
+++ events.js
@@ -2,9 +2,31 @@ export class Events{
     constructor(name, url, location, sale, date, classification){
         this.name = name;
         this.url = url;
-        this.location;
-        this.sale;
-        this.date;
-        this.classification
+        this.location = location;
+        this.sale = sale;
+        this.date = date;
+        this.classification = classification;
     }
 }
+
+export const loadEvents = async (n) => {
+    const response = await fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=IY15m5Po8rivnx1OIfoiX8lYZcYyTgVY&size=2');
+    const data = await response.json();
+
+    const events = data._embedded.events.map(event =>
+        new Events(
+            event.name,
+            event.url,
+            event._embedded.venues[0].name,
+            event.sales.public.startDateTime,
+            event.dates.start.localDate,
+            event.classifications[0].segment.name
+        )
+    );
+
+    return events;
+
+};
+
+const events = await loadEvents(2);
+console.log(events);
