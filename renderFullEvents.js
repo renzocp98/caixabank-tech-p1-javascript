f4c8af88edac323d98d62e4dcc7ca569804a4db3
renzocp98 <renzo0ncp@gmail.com>
2026-03-16 20:28:42 +0100

    Ultimos retoques en el estilo

--- renderFullEvents.js
+++ renderFullEvents.js
@@ -1,6 +1,7 @@
 const header = (title) => `
 <head>
   <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -38,13 +39,15 @@ export const renderEachEvent = (event) => {
     <html>
       ${header(event.name)}
       <body>
-        <a href="index.html">Volver</a>
-        <h1>${event.name}</h1>
-        <img src="${event.image}" />
-        <p>Fecha: ${event.date}</p>
-        <p>Lugar: ${event.location}</p>
-        <p>Categoría: ${event.classification}</p>
-        <a href="${event.url}" target="_blank">Comprar entradas</a>
+      <a href="index.html" class="back">Volver</a>
+        <div class="detail">
+          <h1>${event.name}</h1>
+          <img src="${event.image}" />
+          <p>Fecha: ${event.date}</p>
+          <p>Lugar: ${event.location}</p>
+          <p>Categoría: ${event.classification}</p>
+          <a href="${event.url}" target="_blank">Comprar entradas</a>
+        </div>
       </body>
     </html>`;
 };
